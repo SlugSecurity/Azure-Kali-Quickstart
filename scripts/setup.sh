934e9c83d2368253da933bca4460e179023e9a42
daniel <106453412+0xfbad@users.noreply.github.com>
2023-12-11 18:36:24 -0800

    Update setup.sh

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
+sudo hostnamectl set-hostname slugmachine
+touch ~/.hushlogin
+
 sudo apt update
-sudo apt install kali-desktop-xcfe xrdp zsh -y
+sudo apt install kali-desktop-xfce xrdp -y
 
 sudo systemctl enable xrdp
 echo "exec startxfce4" > ~/.xsession
 sudo service xrdp restart
-
-chsh -s $(which zsh)
