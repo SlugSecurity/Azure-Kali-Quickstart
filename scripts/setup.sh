a34a3b136b133d7262d27c1f46fd40b70c5a5430
daniel <106453412+0xfbad@users.noreply.github.com>
2023-12-11 22:35:09 -0800

    Update setup.sh

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -4,8 +4,8 @@ sudo hostnamectl set-hostname slugbox
 touch ~/.hushlogin
 
 sudo DEBIAN_FRONTEND=noninteractive apt update
-sudo DEBIAN_FRONTEND=noninteractive apt install kali-desktop-kde xrdp -y
+sudo DEBIAN_FRONTEND=noninteractive apt install kali-desktop-xfce xrdp -y
 
 sudo systemctl enable xrdp
-echo "exec startkde" > ~/.xsession
+echo "exec startxfce4" > ~/.xsession
 sudo service xrdp restart
