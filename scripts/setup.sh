61e656053f9873613c6bd5f646c56ef4d86cacf3
daniel <106453412+0xfbad@users.noreply.github.com>
2023-12-15 01:52:07 -0800

    Update setup.sh

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -6,8 +6,6 @@ touch ~/.hushlogin
 sudo DEBIAN_FRONTEND=noninteractive apt update
 sudo DEBIAN_FRONTEND=noninteractive apt install kali-desktop-xfce xrdp -y
 
-echo -e 'Section "Extensions"\n    Option "Composite" "Disable"\nEndSection' | sudo tee /etc/X11/xorg.conf.d/disable-compositing.conf > /dev/null
-
 sudo systemctl enable xrdp
 echo "exec startxfce4" > ~/.xsession
 sudo service xrdp restart
